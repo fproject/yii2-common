0c21a15de6076e576eaf53b8a9a623b06b5ea71d
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-08-21 19:47:45 +0700

    Update test case

--- tests/codeception/config/db.php
+++ tests/codeception/config/db.php
@@ -4,7 +4,7 @@
  */
  return [
 	'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=127.0.0.1;dbname=yii2_restx_test',
+	'dsn' => 'mysql:host=127.0.0.1;dbname=yii2_common_test',
 	'username' => 'root',
 	'password' => '',
 	'charset' => 'utf8',
