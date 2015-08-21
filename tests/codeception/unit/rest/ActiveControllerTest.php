62480d89bfc94b7c7a461434faaf40b90bfaa3a2
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-08-21 19:20:19 +0700

    Add Travis-CI support

---
+++ tests/codeception/unit/rest/ActiveControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace tests\unit\rest;
+
+use fproject\rest\ActiveController;
+use Yii;
+use yii\codeception\TestCase;
+
+class ActiveControllerTest extends TestCase
+{
+	use \Codeception\Specify;
+
+    public function testActions001()
+    {
+    	$this->specify('check ActiveControllerTest\'s actions', function () {
+    		$controller = new ActiveController('user', Yii::$app,
+                [
+                    'modelClass' => 'tests\unit\models\User'
+                ]);
+    		expect("controller id should be 'user'", $controller->id == 'user' )->true();
+    	});
+    }
+}
