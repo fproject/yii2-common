2741dc27864459c8f5cf6947051972fcc4f09e08
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-08-22 11:23:54 +0700

    Update test cases for DbHelper

--- tests/codeception/unit/components/DbHelperTest.php
+++ tests/codeception/unit/components/DbHelperTest.php
@@ -2,6 +2,7 @@
 
 namespace tests\unit\rest;
 
+use tests\unit\models\User;
 use Yii;
 use yii\codeception\TestCase;
 use fproject\components\DbHelper;
@@ -10,9 +11,24 @@ class DbHelperTest extends TestCase
 {
 	use \Codeception\Specify;
 
-    public function testActions001()
+    public function testCreateMultipleUpdateCommand001()
     {
         $cmd = DbHelper::createMultipleUpdateCommand('user',[],'id');
         $this->assertInstanceOf('yii\db\Command', $cmd);
+    }
+
+    public function testBatchSaveCommand001()
+    {
+        $models = [];
+        for($i=0;$i<10;$i++)
+        {
+            $m = $models[] = new User();
+            $m->username = "New User $i-".rand(1000,9999);
+            $m->password = $m->username;
+        }
+        $return = DbHelper::batchSave($models);
+        $this->assertObjectNotHasAttribute('updateCount', $return);
+        $this->assertObjectHasAttribute('insertCount', $return);
+        $this->assertObjectHasAttribute('lastId', $return);
     }
 }
